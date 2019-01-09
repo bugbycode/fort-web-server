7ecac84347f44a0fedbf07ae8ae8fec44c3054d2
zhigongzhang <zhigongzhang@zhigongzhang-PC>
2019-01-09 15:05:59 +0800

    优化提示信息

--- src/main/resources/static/javascript/custom/bugbycode.js
+++ src/main/resources/static/javascript/custom/bugbycode.js
@@ -7,4 +7,19 @@ $(function() {
 　　}
 　　window.history.pushState('forward', null, '#'); //在IE中必须得有这两行
 　　window.history.forward(1);
+
+   $("a").on('hover',function(e) {
+	   var title = $(this).prop('title');
+	   console.log(title);
+		if (title != undefined) {
+			$(this).tooltip({
+				title: title,
+				placement: 'bottom',
+				trigger: 'hover',
+				container: 'body'
+			});
+			$(this).tooltip('show');
+			$(this).attr('data-init', true);
+		}
+	});
 });
